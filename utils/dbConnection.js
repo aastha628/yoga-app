0ee458ebec5d1f76db5823e91964d96070aa6d83
aastha628 <aasthagoyal6.28@gmail.com>
2022-12-12 17:44:43 +0530

    updated database connection

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: true
+    ssl: process.env.ENVIRONMENT == 'dev'
 });
 
 const connectDb = async () => {
